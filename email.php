bb64b9f2ed5a86ccbf46d22a0341c34e4105ac22
krisek4499 <52441938+krisek4499@users.noreply.github.com>
2019-07-22 13:30:16 +0200

    update

--- email.php
+++ email.php
@@ -6,16 +6,7 @@
 $emails=$_POST['email'];
 $info=send::Inf($emails);
 
-/*$instance=connect_DB::getInstance();
-$q = "SELECT email FROM testowa where email='$emails'";
-$stm = $instance->prepare($q);
-$stm->execute();
-$data = $stm->fetchAll(PDO::FETCH_OBJ);
-if($data!=null) $jest="tak";
-else $jest="Formularz wypelniony pomyslnie";
 
-
-echo json_encode($jest);*/
 echo json_encode($info);
 ?>
 
